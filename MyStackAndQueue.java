1045111096fc689f0d91de628ca14a3a6317f2b6
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-10 22:31:29 +0530

    Added:code to peek and pop from stack till it is empty

--- MyStackAndQueue.java
+++ MyStackAndQueue.java
@@ -3,7 +3,6 @@ package assignments.StackAndQueuesProblem;
 import static java.lang.System.exit;
 
 public class MyStackAndQueue {
-     // create global top reference variable global
      Node top;
 
      // Constructor
@@ -11,23 +10,18 @@ public class MyStackAndQueue {
          this.top = null;
      }
 
-     public void push(int x) // insert at the beginning
-     {
+     public void push(int x){ // insert at the beginning
          // create new node temp and allocate memory
          Node temp = new Node();
-
          // check if stack (heap) is full. Then inserting an element would lead to stack overflow
          if (temp == null) {
              System.out.println("Heap Overflow");
              return;
          }
-
          // initialize data into temp data field
          temp.data = x;
-
          // put top reference into temp link
          temp.link = top;
-
          // update top reference
          top = temp;
      }
@@ -50,6 +44,28 @@ public class MyStackAndQueue {
             }
         }
     }
+    public boolean isEmpty() {
+        return top == null;
+    }
+    public int peek() {
+        // check for empty stack
+        if (!isEmpty()) {
+            return top.data;
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+    public void pop() // remove at the beginning
+    {
+        // check for stack underflow
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+        // update the top pointer to point to the next node
+        top = (top).link;
+    }
      public static void main(String[] args) {
         System.out.println("Welcome to stack and queue ");
          // create Object of Implementing class
@@ -62,6 +78,20 @@ public class MyStackAndQueue {
 
          // print Stack elements
          obj.display();
+         // print Top element of Stack
+         System.out.println("\nTop element is :" + obj.peek());
+
+         // Delete top element of Stack
+         obj.pop();
+         obj.pop();
+         obj.pop();
+
+         // print Stack elements
+         obj.display();
+
+         // print Top element of Stack
+         System.out.printf("\nTop element is %d\n",
+                 obj.peek());
 
      }
 
