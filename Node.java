13711f8a4ec49d1477c3f6599afaae370e978cd6
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-10 22:50:36 +0530

    Added:code to create a queue by using linkedlist

--- Node.java
+++ Node.java
@@ -4,4 +4,9 @@ public class Node {
 
     int data; // integer data
     Node link; // reference variable Node type
+
+    public Node(int data) {
+        this.data = data;
+        this.link = null;
+    }
 }
