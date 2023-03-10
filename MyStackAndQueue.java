92d21fa9d1ec7c71c54bda7e571854ef83764370
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-10 20:16:24 +0530

    Passed:welcome message

---
+++ MyStackAndQueue.java
@@ -0,0 +1,9 @@
+package assignments.StackAndQueuesProblem;
+ public class MyStackAndQueue {
+     public static void main(String[] args) {
+        System.out.println("Welcome to stack and queue ");
+    }
+
+
+}
+
